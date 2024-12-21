28ed3f7e067b99261e3b2a1af4f089229008a088
diegoRecinos <00182122@uca.edu.sv>
2024-12-21 09:13:13 -0600

    another config

--- sagarshah.dev/next.config.js
+++ sagarshah.dev/next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: 'export', // Exporta archivos estáticos
   images: {
     unoptimized: true, // Desactiva optimización de imágenes (requerido para GitHub Pages)
   },
-  assetPrefix: './', // Rutas relativas
-  basePath: '', // Asegura que no se rompan rutas en GitHub Pages
+  assetPrefix: './', // Usa rutas relativas para recursos estáticos
+  basePath: '', // Deja vacío ya que estás usando diegoRecinos.github.io
 };
 
 module.exports = nextConfig;
