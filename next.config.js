44e9f168ab57ac1ebb71ee455abb5359cafea58f
diegoRecinos <00182122@uca.edu.sv>
2024-12-21 10:39:49 -0600

    deploy n claude

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  // Remover basePath y assetPrefix si es un repositorio username.github.io
-};
-module.exports = nextConfig;
